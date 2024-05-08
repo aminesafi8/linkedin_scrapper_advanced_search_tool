ce31ba6472da908640df8ef5960a9c3f1248dcd4
Amine Safi <aminesafig@gmail.com>
2024-05-08 20:09:12 +0200

    Update parameters.py
    delete email from code source

--- parameters.py
+++ parameters.py
@@ -1,5 +1,5 @@
 search_query = 'full stack'
-linkedin_username = 'amine.safi@esprit.tn'
+linkedin_username = ''
 linkedin_password = ''
 #to change it dynamically
 cookie='AQEDASTrpskCoZKYAAABbEl5SPIAAAFsbYXM8lYAMR23gmHxx6MRo7WtnvE-u19VM3P_D4lQOZU4mEo0L409SVo1I1_g-mSrbNCpeaYg4ttF9BWme6ZWnRkcJi2afnA7oDn7pGzvBgiFXTtxo_58G-7S'
